f723a6a57bc3e1f2d011e8a65927ca478d5dd0f7
RavendraNand97 <ravendranand97@gmail.com>
2020-09-25 18:50:32 -0400

    Lab 4 Form Completion

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    console.log('Form data in req.body', req.body);
+    res.send("Hello World");
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.send("Hello World");
   });
 
 app.listen(port, () => {
